091bfa2109c60dac2adcd127f2a13e2e9f13b04a
justas <justas2353@gmail.com>
2024-03-07 15:34:39 +0200

    008

---
+++ 008/app.js
@@ -0,0 +1,31 @@
+console.log('Hello from far away!');
+
+let g;
+
+ let a = -50;
+ let b = '';
+ let c = 'Hello';
+
+ let x = 'An';
+ let y = 'tis';
+
+ let l1 = 'B';
+ let l2 = 'a';
+
+
+//  console.log((0.3 + 0.6).toFixed(1)); suapvalina ir nelieka 0.89999...
+// Let skirtas sukurti NAUJAI raidei ir panaudojamas viena karta tai raidei
+
+
+console.log(0.3 + 0.6);
+
+
+a = 50;
+
+console.log(g + b);
+
+let t = true;
+let f = false;
+
+
+console.log(t || f);
