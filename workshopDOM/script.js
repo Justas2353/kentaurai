cd51accf0c9a7c762b1a422e855c259f5ddce9a6
justas <justas2353@gmail.com>
2024-04-12 12:00:50 +0300

    mentor

---
+++ workshopDOM/script.js
@@ -0,0 +1,38 @@
+
+
+const count = document.querySelector('.count')
+const substract = document.querySelector('.substract')
+const reset = document.querySelector('.reset')
+const add = document.querySelector('.add')
+
+
+add.addEventListener('click', function(){
+    count.innerHTML++
+});
+
+
+
+
+const substractFunc = () => {
+    count.innerHTML--
+};
+
+substract.addEventListener('click', substractFunc);
+
+
+
+reset.addEventListener('click', function(){
+    count.innerHTML = 0
+});
+
+
+
+const setColor = () => {
+    if(count.innerHTML > 0) {
+        count.style.color = "yellow"
+    } else if (count.innerHTML < 0) {
+        count.style.color = "red"
+    } else {
+        count.style.color = "#fff"
+    }
+};
