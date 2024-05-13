2880dfa3f9e2dd0f7a670c49ea6d461a1ef7dbe0
justas <justas2353@gmail.com>
2024-05-13 04:16:03 +0300

    039 notlkiethis

--- 039_NODE/index.js
+++ 039_NODE/index.js
@@ -1,12 +1,54 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 80;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
-// app.get('/', (req, res) => {
-//   res.send('Hello 039!');
-// });
+
+app.get('/params', (req, res) => {
+
+  const name = req.query.n;
+  const surname = req.query.s
+
+  res.json({
+    mesage: 'OK',
+    got: {
+      name, surname
+    }
+  });
+});
+
+
+
+app.get('/params/:n/:s', (req, res) => {
+
+  const name = req.params.n;
+  const surname = req.params.s
+
+  res.json({
+    mesage: 'OK',
+    got: {
+      name, surname
+    }
+  });
+});
+
+
+app.post('/body', (req, res) => {
+
+  const name = req.body.name;
+  const surname = req.body.surname;
+
+  res.json({
+    mesage: 'OK',
+    got: {
+      name, surname
+    }
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`039 app listening on port ${port}`);
